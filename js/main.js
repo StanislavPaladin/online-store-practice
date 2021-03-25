abc351ffce4d304e936d1538bdad8fd747859288
Stanislav Paladin <paladinstanis@gmail.com>
2021-03-25 23:22:26 +0300

    feat: created scripts for fill cart/remove goods from cart/calculate price and count items

--- js/main.js
+++ js/main.js
@@ -9,19 +9,163 @@ const mySwiper = new Swiper('.swiper-container', {
 });
 
 
-//cart
+//переменные
 const buttonCart = document.querySelector('.button-cart');
 const modalCart = document.querySelector('#modal-cart');
 const body = document.querySelector('body');
+const more = document.querySelector('.more');
+const navigationLink = document.querySelectorAll('.navigation-link');
+const longGoodsList = document.querySelector('.long-goods-list');
+const cartTableGoods = document.querySelector('.cart-table__goods');
+const cartTableTotal = document.querySelector('.card-table__total');
+const scrollLinks = document.querySelectorAll('.scroll-link');
+const cartCount = document.querySelector('.cart-count');
+const modal = document.querySelector('.modal');
+//запрос к БД
+const getGoods = async () => {
+	const result = await fetch('db/db.json');
+	if (!result.ok) {
+		throw ('ошибка') + result.status;
+	}
+	return await result.json();
+};
 
-function openModal() {
+//основной объект корзины
+const cart = {
+	cartGoods: [],
+	renderCart() {
+		cartTableGoods.textContent = '';
+		this.cartGoods.forEach(({
+			id,
+			name,
+			price,
+			count
+		}) => {
+			const trGood = document.createElement('tr');
+			trGood.className = 'cart-item';
+			trGood.dataset.id = id;
+			trGood.innerHTML = `
+			<td>${name}</td>
+			<td>${price}$</td>
+			<td><button class="cart-btn-minus">-</button></td>
+			<td>${count}</td>
+			<td><button class="cart-btn-plus">+</button></td>
+			<td>${price * count}$</td>
+			<td><button class="cart-btn-delete">x</button></td>
+		</tr>
+			`;
+			cartTableGoods.append(trGood);
+		});
+
+		const totalPrice = this.cartGoods.reduce((sum, item) => {
+			return sum + item.price * item.count;
+		}, 0);
+		cartTableTotal.textContent = totalPrice + '$';
+	},
+	renderGender() {
+		const totalCounter = this.cartGoods.reduce((sum, item) => {
+			return sum + item.count;
+		}, 0);
+
+		cartCount.innerHTML = totalCounter;
+	},
+	deleteGood(id) {
+		this.cartGoods = this.cartGoods.filter(item => id !== item.id);
+		this.renderCart();
+		this.renderGender();
+	},
+	minusGood(id) {
+		for (const item of this.cartGoods) {
+			if (item.id === id) {
+				if (item.count <= 1) {
+					this.deleteGood(id);
+				} else {
+					item.count--;
+					this.renderGender();
+					this.renderCart();
+				}
+				break;
+			}
+
+		}
+	},
+	plusGood(id) {
+		for (const item of this.cartGoods) {
+			if (item.id === id) {
+				item.count++;
+				break;
+			}
+		}
+		this.renderCart();
+		this.renderGender();
+	},
+	addCartGoods(id) {
+		const goodItem = this.cartGoods.find(item => item.id === id);
+		if (!goodItem) {
+			getGoods()
+				.then(data => data.find(item => item.id === id))
+				.then(({
+					id,
+					name,
+					price
+				}) => {
+					this.cartGoods.push({
+						id,
+						name,
+						price,
+						count: 1,
+					});
+					this.renderGender();
+				});
+		} else {
+			this.plusGood(id);
+
+		}
+	},
+};
+cart.renderCart();
+window.body.addEventListener('click', (e) => {
+	const addToCart = e.target.closest('.add-to-cart');
+	if (e.target.closest('.add-to-cart')) {
+
+		cart.addCartGoods(addToCart.dataset.id);
+		cart.renderCart();
+	}
+});
+
+//слушатели внутри корзины, добавление и удаление 
+modal.addEventListener('click', (e) => {
+	const target = e.target;
+	if (target.classList.contains('cart-btn-delete')) {
+		const id = target.closest('.cart-item').dataset.id;
+		cart.deleteGood(id);
+	}
+	if (target.classList.contains('cart-btn-minus')) {
+		const id = target.closest('.cart-item').dataset.id;
+		cart.minusGood(id);
+	}
+	if (target.classList.contains('cart-btn-plus')) {
+		const id = target.closest('.cart-item').dataset.id;
+		cart.plusGood(id);
+	}
+	if (target.classList.contains('clear-cart__btn')) {
+		cart.cartGoods.forEach(function (item) {
+			cart.deleteGood(item.id);
+			closeModal();
+		});
+
+
+	}
+});
+//функции, связанные с модалками
+const openModal = () => {
+	cart.renderCart();
 	modalCart.classList.add('show');
-}
-
-function closeModal() {
+};
+const closeModal = () => {
 	modalCart.classList.remove('show');
-}
-
+};
+//слушатели для открытия и закрытия модалок
 body.addEventListener('click', (e) => {
 	const target = e.target;
 	if (target.matches('.button-cart') || target.matches('[data-cart]')) {
@@ -37,12 +181,8 @@ window.addEventListener('keydown', (e) => {
 		closeModal();
 	}
 });
-
 //scroll smooth
-
-const scrollLinks = document.querySelectorAll('.scroll-link');
-
-function scrollFunction() {
+const scrollFunction = () => {
 	for (const scrollLink of scrollLinks) {
 		scrollLink.addEventListener('click', (event) => {
 			event.preventDefault();
@@ -53,24 +193,11 @@ function scrollFunction() {
 			});
 		});
 	}
-}
-scrollFunction();
-
-//goods
-
-const more = document.querySelector('.more');
-const navigationLink = document.querySelectorAll('.navigation-link');
-const longGoodsList = document.querySelector('.long-goods-list');
-
-const getGoods = async function () {
-	const result = await fetch('db/db.json');
-	if (!result.ok) {
-		throw ('ошибка') + result.status;
-	}
-	return await result.json();
 };
+scrollFunction();
+//первичный рендер карточек
 getGoods().then(function (data) {});
-
+//динамическое создание карточек
 const createCard = function ({
 	label,
 	name,
@@ -81,7 +208,6 @@ const createCard = function ({
 }) {
 	const card = document.createElement('div');
 	card.className = 'col-lg-3 col-sm-6';
-
 	card.innerHTML = `
 		<div class="goods-card">
 		${label ?
@@ -98,7 +224,7 @@ const createCard = function ({
 	`;
 	return card;
 };
-
+//рендер карточек
 const renderCards = function (data) {
 	longGoodsList.textContent = '';
 	const cards = data.map(createCard);
@@ -107,24 +233,21 @@ const renderCards = function (data) {
 	});
 	document.body.classList.add('show-goods');
 };
-more.addEventListener('click', function (event) {
+//слушатель, срабатывающий для view all
+more.addEventListener('click', (event) => {
 	event.preventDefault();
 	scrollFunction();
 	getGoods().then(renderCards);
-	
 });
-
+//фильтрация карточек товаров по типам
 const filterCards = function (field, value) {
-	getGoods().then(function (data) {
-			const result = data.filter(function (good) {
-				return good[field] === value;
-			});
-			return result;
-		})
+	getGoods()
+		.then(data => data.filter(good => good[field] === value))
 		.then(renderCards);
 };
-
+//слушатель, активирующий фильтрацию карточек товаров по типам
 navigationLink.forEach(function (link) {
+	getGoods();
 	link.addEventListener('click', function (event) {
 		event.preventDefault();
 		let field = link.dataset.field;
@@ -136,7 +259,7 @@ navigationLink.forEach(function (link) {
 		}
 	});
 });
-
+//слушатель view all
 const viewAllBtn = document.querySelectorAll('.view-all-btn');
 viewAllBtn.forEach(function (link) {
 	link.addEventListener('click', function (event) {
@@ -151,3 +274,4 @@ viewAllBtn.forEach(function (link) {
 		}
 	});
 });
+getGoods();
